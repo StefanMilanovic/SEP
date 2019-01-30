f7c6fb006bd10c51f1226fd60cec3420bd8aea28
sulepaja <m.s.zoric5@gmail.com>
2019-01-30 15:24:14 +0100

    spring security propo

---
+++ koncentratorPlacanja/src/main/java/koncentratorPlacanja/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package koncentratorPlacanja.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity
+public class SecurityConfig {
+
+}
